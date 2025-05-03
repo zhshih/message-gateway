c12fe6652cda2ead4c9af3332332f3d5b03fb572
Z.H. <galaxyshih@gmail.com>
2025-05-03 18:41:54 +0800

    Refactor process-task in pipeline

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -5,6 +5,7 @@ use anyhow;
 use log::{debug, error, info};
 use std::sync::Arc;
 use tokio::sync::mpsc;
+use tokio::task::JoinHandle;
 use tokio_util::sync::CancellationToken;
 
 pub async fn start_pipeline(
@@ -12,114 +13,47 @@ pub async fn start_pipeline(
     edge_cfg: EdgeConfig,
     shutdown_token: Arc<CancellationToken>,
 ) -> anyhow::Result<()> {
-    let cloud_cfg_clone = cloud_cfg.clone();
-    let (cloud_client, eventloop, mut cloud_source_rx) = MqttClient::new(cloud_cfg_clone);
+    let (cloud_client, eventloop, cloud_source_rx) = MqttClient::new(cloud_cfg.clone());
+    let (edge_client, edge_source_rx) = RedisClient::new(edge_cfg.clone()).await?;
 
-    let edge_cfg_clone = edge_cfg.clone();
-    let (edge_client, mut edge_source_rx) = RedisClient::new(edge_cfg_clone).await?;
-
-    let cloud_cfg_clone = cloud_cfg.clone();
-    let cloud_sub_topics = vec![cloud_cfg_clone.sub_cloud_topic.clone()];
-
-    info!("Start source job");
+    info!("Start source task");
+    let cloud_sub_topics = vec![cloud_cfg.clone().sub_cloud_topic.clone()];
     cloud_client.subscribe(cloud_sub_topics).await?;
 
-    let edge_cfg_clone = edge_cfg.clone();
-    let edge_sub_topics = vec![edge_cfg_clone.sub_edge_topic.clone()];
+    let edge_sub_topics = vec![edge_cfg.clone().sub_edge_topic.clone()];
     let pubsub_conn = edge_client.subscribe(edge_sub_topics).await?;
 
-    let cloud_sub_topics = vec![cloud_cfg_clone.sub_cloud_topic.clone()];
-    let shutdown_token_clone = shutdown_token.clone();
+    let cloud_sub_topics = vec![cloud_cfg.clone().sub_cloud_topic.clone()];
     let cloud_source_task = tokio::spawn(MqttClient::handle_events(
         cloud_client.client(),
         eventloop,
         cloud_client.event_sender(),
         cloud_sub_topics,
-        shutdown_token_clone,
+        shutdown_token.clone(),
     ));
 
-    let shutdown_token_clone = shutdown_token.clone();
     let edge_source_task = tokio::spawn(RedisClient::handle_messages(
         pubsub_conn,
         edge_client.message_sender(),
-        shutdown_token_clone,
+        shutdown_token.clone(),
     ));
 
-    let (cloud_processed_tx, mut cloud_processed_rx) = mpsc::channel::<String>(32);
-    let shutdown_token_clone = shutdown_token.clone();
-    let cloud_process_task = tokio::spawn(async move {
-        info!("Launching cloud-process task");
-        let mut seq_id = 0;
+    info!("Start process task");
+    let (cloud_processed_tx, mut cloud_processed_rx) = mpsc::channel(32);
+    let cloud_process_task = spawn_processor(
+        "cloud",
+        cloud_source_rx,
+        cloud_processed_tx,
+        shutdown_token.clone(),
+    );
 
-        loop {
-            tokio::select! {
-                _ = shutdown_token_clone.cancelled() => {
-                    debug!("cloud-process task received shutdown signal.");
-                    break;
-                }
-                maybe_msg = cloud_source_rx.recv() => {
-                    match maybe_msg {
-                        Some(msg) => {
-                            debug!("Received source message: {msg}");
-                            seq_id += 1;
-                            let processed_msg = format!("{} seq_id = {}", msg, seq_id);
-
-                            debug!("Sending processed message: {processed_msg}");
-                            if let Err(e) = cloud_processed_tx.send(processed_msg.clone()).await {
-                                error!("Failed to send processed message: {:?}", e);
-                            } else {
-                                debug!("Sent processed message: {processed_msg}");
-                            }
-                        }
-                        None => {
-                            info!("Source channel closed.");
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        debug!("Exiting cloud-process task.");
-    });
-
-    let (edge_processed_tx, mut edge_processed_rx) = mpsc::channel::<String>(32);
-    let shutdown_token_clone = shutdown_token.clone();
-    let edge_process_task = tokio::spawn(async move {
-        info!("Launching edge-process task");
-        let mut seq_id = 0;
-
-        loop {
-            tokio::select! {
-                _ = shutdown_token_clone.cancelled() => {
-                    debug!("edge-process task received shutdown signal.");
-                    break;
-                }
-                maybe_msg = edge_source_rx.recv() => {
-                    match maybe_msg {
-                        Some(msg) => {
-                            debug!("Received source message: {msg}");
-                            seq_id += 1;
-                            let processed_msg = format!("{} seq_id = {}", msg, seq_id);
-
-                            debug!("Sending processed message: {processed_msg}");
-                            if let Err(e) = edge_processed_tx.send(processed_msg.clone()).await {
-                                error!("Failed to send processed message: {:?}", e);
-                            } else {
-                                debug!("Sent processed message: {processed_msg}");
-                            }
-                        }
-                        None => {
-                            info!("Source channel closed.");
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        debug!("Exiting edge-process task.");
-    });
+    let (edge_processed_tx, mut edge_processed_rx) = mpsc::channel(32);
+    let edge_process_task = spawn_processor(
+        "edge",
+        edge_source_rx,
+        edge_processed_tx,
+        shutdown_token.clone(),
+    );
 
     let shutdown_token_clone = shutdown_token.clone();
     let cloud_sink_task = tokio::spawn(async move {
@@ -197,4 +131,36 @@ pub async fn start_pipeline(
     edge_sink_task.await?;
 
     Ok(())
+}
+
+fn spawn_processor(
+    name: &str,
+    mut source_rx: mpsc::Receiver<String>,
+    processed_tx: mpsc::Sender<String>,
+    shutdown_token: Arc<CancellationToken>,
+) -> JoinHandle<()> {
+    let name = name.to_string();
+    tokio::spawn(async move {
+        info!("Launching {name}-processor task");
+        let mut seq_id = 0;
+
+        loop {
+            tokio::select! {
+                _ = shutdown_token.cancelled() => {
+                    debug!("{name}-processor received shutdown signal");
+                    break;
+                }
+                Some(msg) = source_rx.recv() => {
+                    seq_id += 1;
+                    let processed = format!("{msg} seq_id = {seq_id}");
+                    debug!("Processed message from {name}: {processed}");
+                    if processed_tx.send(processed).await.is_err() {
+                        error!("{name} processor failed to send message");
+                        break;
+                    }
+                }
+                else => break,
+            }
+        }
+    })
 }
